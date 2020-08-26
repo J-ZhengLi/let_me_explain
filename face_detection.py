d6226f4d96e3f93fa0032132209be5bbd9fc3369
Jonathan Lee <jlee@broccoli.fios-router.home>
2020-08-26 19:49:18 -0400

    Face detecting works, timer works

--- face_detection.py
+++ face_detection.py
@@ -1,4 +1,6 @@
 import cv2
+#import time
+from threading import Timer
 from enum import Enum
 
 class MediaType(Enum):
@@ -8,30 +10,48 @@ class MediaType(Enum):
     CAMERA = 3
 
 class FaceDetection:
-    
-    def __init__(self, cascadeFp='openCV_data/haarcascade_frontalface_default.xml', factor=1.2):
+    faces = []
+    def __init__(self, cascadeFp='openCV_data/haarcascade_frontalface_alt.xml', factor=1.2):
         self.cascadeFile = cascadeFp
         self.factor = factor
 
     def detect(self, mediaType):
         # WHY PYTHON DOESN'T HAVE A PROPER SWITCH STATEMENT!
         if mediaType == MediaType.STATIC_IMAGE:
-            print('Unsupport yet')
+            print('Unsupported yet')
         elif mediaType == MediaType.VIDEO:
-            print('Unsupport yet')
+            print('Unsupported yet')
         elif mediaType == MediaType.CAMERA:
             # capture video from web cam
             capture = cv2.VideoCapture(0)
 
             # check if theres a camera attached
             if capture != None and capture.isOpened():
+                frame_skip = 0
                 while(True):
                     # get image frame by frame
                     _, frame = capture.read()
 
-                    # DEBUG only: Show captured video frame by frame
-                    cv2.imshow('Debug, Press Q to quit', frame)
+                    # skip frames to optimize cpu usage
+                    frame_skip = (frame_skip + 1) % 2
 
+                    if frame_skip == 1:
+                        # Begain face detection
+                        cascade = cv2.CascadeClassifier(self.cascadeFile)
+                        # convert this frame to grayscale to make detection easier
+                        g_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        self.faces = cascade.detectMultiScale(g_frame, self.factor)
+                        # End face detection
+
+                    # DEBUG only: Show captured video frame by frame
+                    # mark detected faces on debug output
+                    if len(self.faces) > 1:
+                        t = Timer(2, self.test)
+                        t.start()
+
+                    for (centerX, centerY, width, height) in self.faces:
+                        cv2.circle(frame, (centerX + int(width/2), centerY + int(height/2)), width, (0, 0, 255))
+                    cv2.imshow('Debuging, Press Q to quit', frame)
                     if cv2.waitKey(1) == ord('q'):
                         break
 
@@ -39,3 +59,6 @@ class FaceDetection:
                 cv2.destroyAllWindows()
             else:
                 print('Cannot find a working webcam')
+                
+    def test(self):
+        print('it is time')
